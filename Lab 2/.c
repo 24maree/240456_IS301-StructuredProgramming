a1a429b414433d9db08992350251cc944d5830c0
24mars <2047mars@gmail.com>
2026-03-23 23:18:13 +1000

    Updated lab

--- Lab 2/.c
+++ Lab 2/.c
@@ -1,44 +1,46 @@
-/* Name: Mareerose Possi  Student ID: 240456  Date: 12/03/26 */
+/* Name: Mareerose Possi    Student ID: 240456  Date: 12/03/26*/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
 // Function Prototypes
-int getGuess();
+int getGuess(int attempts);
 void printSuccess();
 void printHint(int guess, int randomNum);
 void printSorry(int randomNum);
 
 int main() {
     int randomNum, guess, attempts;
-    srand(time(0)); // Seed for random number generation
-    randomNum = (rand() % 20) + 1; // Generate number 1-20
+
+    srand(time(0));
+    randomNum = (rand() % 20) + 1;
 
     printf("I am thinking of a number between 1 and 20.\n");
 
-    // Repetition Logic: 5 tries
     for (attempts = 1; attempts <= 5; attempts++) {
-        guess = getGuess();
+        guess = getGuess(attempts);
 
-        // Selection Logic
         if (guess == randomNum) {
             printSuccess();
-            return 0; // Terminate immediately after correct guess
+            return 0;
         } else if (attempts < 5) {
             printHint(guess, randomNum);
         }
     }
 
-    // If 5 attempts fail
     printSorry(randomNum);
     return 0;
 }
 
 // Function to get user guesses
-int getGuess() {
+int getGuess(int attempts) {
     int g;
-    printf("Can you guess what it is? ");
+    if (attempts == 1) {
+        printf("Can you guess what it is? ");
+    } else {
+        printf("Try again: ");
+    }
     scanf("%d", &g);
     return g;
 }
@@ -48,17 +50,18 @@ void printSuccess() {
     printf("Congratulations! You did it.\n");
 }
 
-// Function to print unsuccessful/hint message
+// Function to print hint message
 void printHint(int guess, int randomNum) {
     if (guess > randomNum) {
-        printf("Your guess is high. Try again:\n");
+        printf("Your guess is high.\n");
     } else {
-        printf("Your guess is low. Try again:\n");
+        printf("Your guess is low.\n");
     }
 }
 
 // Function to print sorry message
 void printSorry(int randomNum) {
     printf("Sorry. The number was %d.\n", randomNum);
-    printf("You should have gotten it by now. Better luck next time.\n");
+    printf("You should have gotten it by now.\n");
+    printf("Better luck next time.\n");
 }
